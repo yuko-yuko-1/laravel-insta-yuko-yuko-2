77a30d35232107b1093fa7753295c81e4d0bcf03
yukonagata <ykubo8@gmail.com>
2024-07-16 00:36:01 +0900

    Fix TrustProxies headers definition

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -22,7 +22,7 @@ class TrustProxies extends Middleware
      * @var int
      */
     // protected $headers =
-    protected $headers = Request::HEADER_X_FORWARDED_ALL|
+    protected $headers = 
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
