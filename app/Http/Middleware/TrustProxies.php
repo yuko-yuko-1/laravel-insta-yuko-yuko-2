e830cdb4072d22c860b04229da1a58dce6a5606f
yukonagata <ykubo8@gmail.com>
2024-07-16 00:19:34 +0900

    Update TrustProxies.php to fix headers

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,14 +12,17 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    // protected $proxies;
+    protected $proxies = '*';
+    
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers =
+    // protected $headers =
+    protected $headers = Request::HEADER_X_FORWARDED_ALL|
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
